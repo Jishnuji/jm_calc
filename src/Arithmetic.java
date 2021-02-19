0231195169000a414efd35b272d2214a6e903897
Jishnuji <73687889+Jishnuji@users.noreply.github.com>
2021-02-19 14:23:18 +0700

    Update Arithmetic.java

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,35 +1,31 @@
 import java.util.HashMap;
 
 public class Arithmetic {
-    private static HashMap<Integer, String> map = new HashMap<>();
-    private static HashMap<String, Integer> map2 = new HashMap<>();
+    private static HashMap<Integer, String> map = new HashMap<>() {{
+        put(100, "C");
+        put(90, "XC");
+        put(40, "XL");
+        put(10, "X");
+        put(9, "IX");
+        put(5, "V");
+        put(4, "IV");
+        put(1, "I");
+        put(0, "0");
+    }};
+    private static HashMap<String, Integer> map2 = new HashMap<>() {{
+        put("I", 1);
+        put("II", 2);
+        put("III", 3);
+        put("IV", 4);
+        put("V", 5);
+        put("VI", 6);
+        put("VII", 7);
+        put("VIII", 8);
+        put("IX", 9);
+        put("X", 10);
+    }};
 
-    private static void fillRomanAndArabic() {
-        map.put(100, "C");
-        map.put(90, "XC");
-        map.put(40, "XL");
-        map.put(10, "X");
-        map.put(9, "IX");
-        map.put(5, "V");
-        map.put(4, "IV");
-        map.put(1, "I");
-
-        map2.put("I", 1);
-        map2.put("II", 2);
-        map2.put("III", 3);
-        map2.put("IV", 4);
-        map2.put("V", 5);
-        map2.put("VI", 6);
-        map2.put("VII", 7);
-        map2.put("VIII", 8);
-        map2.put("IX", 9);
-        map2.put("X", 10);
-    }
-
-    static {
-        fillRomanAndArabic();
-    }
-
+    /** Конвертируем полученный результат в римскую цифру */
     private static String toRoman(int n) {
         StringBuilder result = new StringBuilder();
 
@@ -54,6 +50,7 @@ public class Arithmetic {
         }
         return result.toString();
     }
+    /** Выполняем арифметические действия с араюскими числами */
     public static int sum(int a, int b) {
         return a + b;
     }
@@ -66,6 +63,7 @@ public class Arithmetic {
     public static int divide(int a, int b) {
         return a / b;
     }
+    /** Выполняем арифметические действия с римскими числами */
     public static String sum(String a, String b) {
         int a2 = map2.get(a);
         int b2 = map2.get(b);
@@ -94,5 +92,4 @@ public class Arithmetic {
         int s = divide(a2, b2);
         return toRoman(s);
     }
-
 }
